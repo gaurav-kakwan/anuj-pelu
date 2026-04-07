55bfad21612d0815691c28541d7417ca13769243
gaurav-kakwan <balajikakawan@gmail.com>
2026-04-07 17:54:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ function validateSession(token) {
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    if (username === "admin" && password === "kakwan") {
+    if (username === "admin" && password === "2026") {
         cleanupExpiredSessions();
         if (activeSessions.size >= MAX_USERS) {
             return res.json({ success: false, msg: "User Limit Reached! Max " + MAX_USERS + " users allowed." });
